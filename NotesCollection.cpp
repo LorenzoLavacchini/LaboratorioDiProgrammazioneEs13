9f147d2feb00a909858ac9e1f7b9575fa718b324
thelavax <lory.lavaxservice@gmail.com>
2021-09-05 11:31:27 +0200

    Aggiunta classe NotesCollection

---
+++ NotesCollection.cpp
@@ -0,0 +1,23 @@
+//
+// Created by lorenzo on 05/09/2021.
+//
+
+#include "NotesCollection.h"
+
+const std::string &NotesCollection::getName() const {
+    return name;
+}
+
+void NotesCollection::setName(const std::string &name) {
+    NotesCollection::name = name;
+}
+
+const std::list<Note> &NotesCollection::getNotes() const {
+    return notes;
+}
+
+void NotesCollection::setNotes(const std::list<Note> &notes) {
+    NotesCollection::notes = notes;
+}
+
+NotesCollection::NotesCollection(std::string n) : name(n){}
---
+++ NotesCollection.h
@@ -0,0 +1,26 @@
+//
+// Created by lorenzo on 05/09/2021.
+//
+
+#ifndef LABORATORIODIPROGRAMMAZIONEES13_NOTESCOLLECTION_H
+#define LABORATORIODIPROGRAMMAZIONEES13_NOTESCOLLECTION_H
+#include "Note.h"
+#include <list>
+
+class NotesCollection {
+private:
+    std::string name;
+    std::list<Note> notes;
+public:
+    NotesCollection(std::string n);
+    const std::string &getName() const;
+
+    void setName(const std::string &name);
+
+    const std::list<Note> &getNotes() const;
+
+    void setNotes(const std::list<Note> &notes);
+};
+
+
+#endif //LABORATORIODIPROGRAMMAZIONEES13_NOTESCOLLECTION_H
