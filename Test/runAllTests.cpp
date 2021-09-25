d8afebdad62843121a6864488872bfbbb07e0fff
thelavax <lory.lavaxservice@gmail.com>
2021-09-25 09:30:47 +0200

    Scrittura del file CMakeLists.txt. Aggiunta di runAllTests.cpp

---
+++ Test/runAllTests.cpp
@@ -0,0 +1,9 @@
+//
+// Created by lorenzo on 25/09/2021.
+//
+#include <gtest/gtest.h>
+
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
