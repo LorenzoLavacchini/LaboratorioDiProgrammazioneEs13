2f6c602a09cdbb27fee1350652fb0aa9c4e8a74f
thelavax <lory.lavaxservice@gmail.com>
2021-09-25 16:11:39 +0200

    Test per verificare il blocco alla modifica delle note se l'attributo blocked è true.

--- Test/NoteTest.cpp
+++ Test/NoteTest.cpp
@@ -11,3 +11,17 @@ TEST(Note, constructor){
     ASSERT_EQ(false, n.isBlocked());
 }
 
+TEST(Note, notBlockedModificationAdmitted){
+    Note n("title","text",false);
+    n.setTitle("new title");
+    n.setText("new text");
+    ASSERT_EQ("new title",n.getTitle());
+    ASSERT_EQ("new text", n.getText());
+}
+TEST(Note, blockedModificationDenied){
+    Note n2("title","text",true);
+    n2.setTitle("new title");
+    n2.setText("new text");
+    ASSERT_EQ("title",n2.getTitle());
+    ASSERT_EQ("text",n2.getText());
+}
