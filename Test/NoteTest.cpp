20af4ac69ed1d2b39a2560658bc646bca7ba325e
thelavax <lory.lavaxservice@gmail.com>
2021-09-25 15:52:32 +0200

    Completamento di NoteTest.cpp

--- Test/NoteTest.cpp
+++ Test/NoteTest.cpp
@@ -4,7 +4,10 @@
 
 #include <gtest/gtest.h>
 #include "../Note.h"
-TEST(Note, getterTitle){
+TEST(Note, constructor){
     Note n("title","text",false);
     ASSERT_EQ("title",n.getTitle());
+    ASSERT_EQ("text",n.getText());
+    ASSERT_EQ(false, n.isBlocked());
 }
+
