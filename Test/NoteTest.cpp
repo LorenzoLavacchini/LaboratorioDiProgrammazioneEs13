cc5e0f15df2911527d51f5159db48b4196e238ff
thelavax <lory.lavaxservice@gmail.com>
2021-09-27 17:02:39 +0200

    Modifica di indentazione per miglior lettura del codice

--- Test/NoteTest.cpp
+++ Test/NoteTest.cpp
@@ -19,9 +19,9 @@ TEST(Note, notBlockedModificationAdmitted){
     ASSERT_EQ("new text", n.getText());
 }
 TEST(Note, blockedModificationDenied){
-    Note n2("title","text",true);
-    n2.setTitle("new title");
-    n2.setText("new text");
-    ASSERT_EQ("title",n2.getTitle());
-    ASSERT_EQ("text",n2.getText());
+    Note n("title","text",true);
+    n.setTitle("new title");
+    n.setText("new text");
+    ASSERT_EQ("title",n.getTitle());
+    ASSERT_EQ("text",n.getText());
 }
