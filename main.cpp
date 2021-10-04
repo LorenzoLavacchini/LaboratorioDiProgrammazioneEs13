a0ab285db36425cc192320a6e1167c5d8bff3bd2
thelavax <lory.lavaxservice@gmail.com>
2021-10-04 19:31:12 +0200

    Modifica al contenuto del main.cpp

--- main.cpp
+++ main.cpp
@@ -10,39 +10,6 @@
 #include <gmock/gmock.h>
 
 int main() {
-    /*NotesCollection notesCollection("Appunti da finire");
-    ImportantNotesCollection importantNotesCollection;
-    SpecificNotesCollection specificNotesCollection;
-
-    //Pattern è in versione pull
-    NotesCollectionView notesCollectionView(&notesCollection);
-    ImportantNotesCollectionView importantNotesCollectionView(&importantNotesCollection);
-    SpecificNotesCollectionView specificNotesCollectionView(&specificNotesCollection);
-
-
-    Note primaNota("prima nota","appunti di informatica",true);
-    Note secondaNota("seconda nota","appunti di matematica",false);
-
-    //Aggiunta di note ad una delle collezioni
-    notesCollection.addNote(&primaNota);
-    notesCollection.addNote(&secondaNota);
-
-    importantNotesCollection.addNote(&primaNota);
-    importantNotesCollection.addNote(&secondaNota);
-
-    specificNotesCollection.addNote(&primaNota);
-    specificNotesCollection.addNote(&secondaNota);
-
-    //Modifica del titolo sulla base dell'attrobuto blocked
-    primaNota.setTitle("nuovo titolo");
-
-    //Test della cancellazione di una nota dalla collezione sulla base dell'attributo blocked
-    notesCollection.printAllNotes();
-    notesCollection.removeNote(&primaNota);
-    notesCollection.removeNote(&secondaNota);
-    notesCollection.printAllNotes();
-    */
     ::testing::InitGoogleTest();
     return RUN_ALL_TESTS();
-
 }
